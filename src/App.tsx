a3687e4eaabcfe09a0786004186bbc7eb21e09cf
Mizan-Rifat <mizanrifat001@gmail.com>
2023-02-12 11:31:52 +0600

    assets added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button variant="phoenix-primary">Click</Button>
+    </div>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './assets/scss/theme.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
