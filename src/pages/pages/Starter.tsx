1e9a2d773834d255e1a8ee7a7d133df539a5d59e
Mizan-Rifat <mizanrifat001@gmail.com>
2023-03-21 11:58:12 +0600

    starter page

--- src/pages/pages/Starter.tsx
+++ src/pages/pages/Starter.tsx
@@ -1,22 +1,25 @@
-import PhoenixDocCard from 'components/base/PhoenixDocCard';
-
-const code = `() => (
-  <h3>
-    So functional. Much wow!
-  </h3>
-)
-`;
+import React from 'react';
+import { Stack } from 'react-bootstrap';
+import starterImg from 'assets/img/spot-illustrations/2.png';
+import starterDarkImg from 'assets/img/spot-illustrations/dark_2.png';
 
 const Starter = () => {
   return (
-    <div>
-      <PhoenixDocCard>
-        <PhoenixDocCard.Header title="State">
-          <h5>jhaskhjdashk</h5>
-        </PhoenixDocCard.Header>
-        <PhoenixDocCard.Body code={code} />
-      </PhoenixDocCard>
-    </div>
+    <Stack className="flex-center content-min-h">
+      <div className="text-center py-9">
+        <img src={starterImg} width={470} className="img-fluid mb-7 d-dark-none" alt="starter" />
+        <img
+          src={starterDarkImg}
+          width={470}
+          className="img-fluid mb-7 d-light-none"
+          alt="starter"
+        />
+        <h1 className="text-800 fw-normal mb-5">Create Something Beautiful.</h1>
+        <a className="btn btn-lg btn-primary" href="../documentation/getting-started.html">
+          Getting Started
+        </a>
+      </div>
+    </Stack>
   );
 };
 
