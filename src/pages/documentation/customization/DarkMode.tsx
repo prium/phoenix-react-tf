2d65157d74988ea1a04aa910a95b629cca9a2a42
Mizan-Rifat <mizanrifat001@gmail.com>
2023-03-21 10:20:55 +0600

    dark mode

--- src/pages/documentation/customization/DarkMode.tsx
+++ src/pages/documentation/customization/DarkMode.tsx
@@ -1,120 +1,21 @@
-// @ts-nocheck
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PhoenixDocCard from 'components/base/PhoenixDocCard';
 import DocPageHeader from 'components/docs/DocPageHeader';
 import DocPagesLayout from 'components/layouts/DocPagesLayout';
 import { useAppContext } from 'providers/AppProvider';
-import { Col, Form, OverlayTrigger, Row, Tooltip } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import FeatherIcon from 'feather-icons-react';
 
-const darkModeExampleCode = `function DarkModeExample(){
-  const {
-    config: { isDark },
-    setConfig
-  } = React.useContext(AppContext); // import AppContext from 'src/context/Context.js'
-  return (
-    <Row>
-      <Col sm={6} lg={3}>
-        <h5 className="fs-0 mb-2">Checkbox </h5>
-        <Form.Check
-          type="checkbox"
-          id="themeToggleCheckBox"
-          label="Dark mode"
-          onChange={e => setConfig('isDark', e.target.checked)}
-          checked={isDark}
-        />
-      </Col>
-      <Col sm={6} lg={3}>
-        <h5 className="fs-0 mb-2">Switch Input</h5>
-        <Form.Check
-          type="switch"
-          id="themeToggleSwitch"
-          label="Dark mode"
-          onChange={e => setConfig('isDark', e.target.checked)}
-          checked={isDark}
-        />
-      </Col>
-      <Col sm={6} lg={3}>
-        <h5 className="fs-0 mb-2">Radio Button</h5>
-        <Form.Group>
-          <Form.Check
-            inline
-            type="radio"
-            id="lightRadioButton"
-            label="Light"
-            name="themeToggleRadio"
-            checked={!isDark}
-            onChange={() => setConfig('isDark', false)}
-          />
-          <Form.Check
-            inline
-            type="radio"
-            id="darkRadioButton"
-            label="Dark"
-            name="themeToggleRadio"
-            checked={isDark}
-            onChange={() => setConfig('isDark', true)}
-          />
-        </Form.Group>
-      </Col>
-      <Col sm={6} lg={3}>
-        <h5 className="fs-0 mb-2">Custom Icon</h5>
-        <div className="px-0 theme-control-toggle" onClick={() => setConfig('isDark', !isDark)}>
-          <OverlayTrigger
-            placement="right"
-            overlay={
-              <Tooltip id="hi">{isDark ? 'Switch to light theme' : 'Switch to dark theme'}</Tooltip>
-            }
-          >
-            <div className="theme-control-toggle-label">
-              <FontAwesomeIcon
-                icon={isDark ? 'sun' : 'moon'}
-                transform="shrink-7"
-                className="fs-4"
-              />
-            </div>
-          </OverlayTrigger>
-        </div>
-      </Col>
-    </Row>
-  );
-};`;
-
-const DarkMode = () => {
-  return (
-    <div>
-      <DocPageHeader title="Dark Mode">
-        <p className="lead text-700">
-          It’s effortless to switch Dark Mode in {process.env.REACT_APP_TITLE}-React. You can enable
-          Dark Mode by default or create a Dark/Light switch if you want. To set the default mode
-          "Dark", please see the
-          <Link to="/documentation/customization/configuration"> configuration page</Link>
-        </p>
-      </DocPageHeader>
-      <DocPagesLayout>
-        <PhoenixDocCard className="mb-4">
-          <PhoenixDocCard.Header
-            title="Toggle Dark Mode"
-            description={`Toggling dark mode is very easy in ${process.env.REACT_APP_TITLE}-React. You can toggle dark or light mode by using checkbox, radio input, switch input and custom icon component.`}
-          />
-          {/* <PhoenixDocCard.Body code={exampleCode} /> */}
-
-          <DarkModeExample />
-        </PhoenixDocCard>
-      </DocPagesLayout>
-    </div>
-  );
-};
-
-export default DarkMode;
+const darkModeExampleCode = `
+import { useAppContext } from 'providers/AppProvider';
+import FeatherIcon from 'feather-icons-react';
 
 const DarkModeExample = () => {
   const {
     config: { theme },
     setConfig
   } = useAppContext(); // import AppContext from 'src/context/Context.js'
-
+  
   return (
     <Row>
       <Col sm={6} lg={3}>
@@ -127,6 +28,7 @@ const DarkModeExample = () => {
           checked={theme === 'dark'}
         />
       </Col>
+
       <Col sm={6} lg={3}>
         <h5 className="fs-0 mb-2">Switch Input</h5>
         <Form.Check
@@ -161,26 +63,157 @@ const DarkModeExample = () => {
           />
         </Form.Group>
       </Col>
-      {/*
+
       <Col sm={6} lg={3}>
         <h5 className="fs-0 mb-2">Custom Icon</h5>
-        <div className="px-0 theme-control-toggle" onClick={() => setConfig('isDark', !isDark)}>
+        <div
+          className="theme-control-toggle"
+          onClick={() => setConfig({ theme: theme === 'light' ? 'dark' : 'light' })}
+        >
           <OverlayTrigger
-            placement="right"
+            placement="left"
             overlay={
-              <Tooltip id="hi">{isDark ? 'Switch to light theme' : 'Switch to dark theme'}</Tooltip>
+              <Tooltip id="ThemeColor">
+                {theme === 'dark' ? 'Switch to light theme' : 'Switch to dark theme'}
+              </Tooltip>
             }
           >
             <div className="theme-control-toggle-label">
-              <FontAwesomeIcon
-                icon={isDark ? 'sun' : 'moon'}
-                transform="shrink-7"
-                className="fs-4"
-              />
+              <FeatherIcon icon={theme === 'dark' ? 'moon' : 'sun'} size={16} />
             </div>
           </OverlayTrigger>
         </div>
-      </Col> */}
+      </Col>
     </Row>
   );
 };
+render(<DarkModeExample />);
+`;
+
+const lightCode = `
+  <Card bg='100' className="light">
+    <Card.Body>
+      <p className="mb-0 text-700">
+        <b>This element will retain it's color if you switch between light and dark mode.</b>
+      </p>
+    </Card.Body>
+  </Card>
+`;
+
+const darkCode = `
+  <Card bg='100' className="dark">
+    <Card.Body>
+      <p className="mb-0 text-700">
+        <b>This element will retain it's color if you switch between light and dark mode.</b>
+      </p>
+    </Card.Body>
+  </Card>
+`;
+
+const dark__Code = `
+  <Card bg='light' className="dark__bg-primary">
+    <Card.Body>
+      <p className="mb-0 dark__text-white">
+        <span className="fw-bold">
+          The background will be primary and text color will be white in dark mode.
+        </span>
+      </p>
+    </Card.Body>
+  </Card>
+`;
+
+const DarkMode = () => {
+  return (
+    <div>
+      <DocPageHeader title="Dark Mode">
+        <p className="lead text-700">
+          It’s effortless to switch Dark Mode in {process.env.REACT_APP_TITLE}-React. You can enable
+          Dark Mode by default or create a Dark/Light switch if you want. To set the default mode
+          "Dark", please see the
+          <Link to="/documentation/customization/configuration"> configuration page</Link>
+        </p>
+      </DocPageHeader>
+      <DocPagesLayout>
+        <PhoenixDocCard className="mb-4">
+          <PhoenixDocCard.Header
+            title="Toggle Dark Mode"
+            description={`Toggling dark mode is very easy in ${process.env.REACT_APP_TITLE}-React. You can toggle dark or light mode by using checkbox, radio input, switch input and custom icon component.`}
+          />
+          <PhoenixDocCard.Body
+            code={darkModeExampleCode}
+            noInline
+            scope={{ FeatherIcon, useAppContext }}
+          />
+        </PhoenixDocCard>
+
+        <PhoenixDocCard className="mb-4">
+          <PhoenixDocCard.Header title="Modify Dark colors" noPreview />
+          <PhoenixDocCard.Body>
+            <p className="mb-0 text-800">
+              You can find all the variables used to create the dark mode in
+              <code> src/assets/scss/theme/root/_dark.scss </code> file. If you want to override a
+              variable, copy that variable to <code> src/assets/scss/_user-variables.scss </code>{' '}
+              file and update it as you see fit. After changing the variable build your scss again.
+            </p>
+          </PhoenixDocCard.Body>
+        </PhoenixDocCard>
+
+        <PhoenixDocCard noProvider className="mb-4">
+          <PhoenixDocCard.Header title="Using the Dark className" noPreview />
+          <PhoenixDocCard.Body>
+            <h6 className="fs-8 mb-3">
+              You can keep a style constant regardless of current (light or dark) mode
+            </h6>
+            <p className="mb-2">
+              If you want a component to retain it’s color (light or dark) as it is regardless of
+              the current mode, you can use the following classeNames -
+            </p>
+            <p className="mb-2">
+              <code> .light </code> - It will keep the color light even if the current mode is dark
+            </p>
+            <p>
+              <code> .dark </code> - It will keep the color dark even if the current mode is light
+            </p>
+
+            <Row className="mb-5">
+              <Col sm={6}>
+                <PhoenixDocCard>
+                  <PhoenixDocCard.Header />
+                  <PhoenixDocCard.Body code={lightCode} />
+                </PhoenixDocCard>
+              </Col>
+              <Col sm={6}>
+                <PhoenixDocCard>
+                  <PhoenixDocCard.Header />
+                  <PhoenixDocCard.Body code={darkCode} />
+                </PhoenixDocCard>
+              </Col>
+            </Row>
+
+            <h6 className="fs-8 mb-3">Override Background and Text color only for dark mode</h6>
+            <p className="mb-2">
+              If you want to use a different text color or background color rather than the default
+              dark theme color for any element, you can use the special "dark" classeNames:
+            </p>
+            <ul className="mb-2">
+              <li>
+                <code>dark__bg-* </code>
+              </li>
+              <li>
+                <code>dark__text-* </code>
+              </li>
+            </ul>
+            <p>The following element illustrates the example:</p>
+
+            <PhoenixDocCard className="mb-4">
+              <PhoenixDocCard.Header />
+              <PhoenixDocCard.Body code={dark__Code} />
+            </PhoenixDocCard>
+          </PhoenixDocCard.Body>
+        </PhoenixDocCard>
+      </DocPagesLayout>
+    </div>
+  );
+};
+
+export default DarkMode;
