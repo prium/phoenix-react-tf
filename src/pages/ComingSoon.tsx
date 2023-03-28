7503a4855ed039e4fe7cf79d0da043acbe43e012
Mizan-Rifat <mizanrifat001@gmail.com>
2023-03-28 12:44:55 +0600

    coming soon page done

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -1,61 +1,66 @@
-import React from 'react';
-import { Col, Row, Stack } from 'react-bootstrap';
-import starterImg from 'assets/img/spot-illustrations/2.png';
-import starterDarkImg from 'assets/img/spot-illustrations/dark_2.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Col, Row } from 'react-bootstrap';
 import Lottie from 'lottie-react';
 import comingSoonLight from 'assets/img/animated-icons/coming-soon-light.json';
-import Button from 'components/base/Button';
+import comingSoonDark from 'assets/img/animated-icons/coming-soon-dark.json';
+import comingSoonText from 'assets/img/spot-illustrations/39.png';
+import comingSoonTextDark from 'assets/img/spot-illustrations/dark_39.png';
+import { useAppContext } from 'providers/AppProvider';
+import { Link } from 'react-router-dom';
 
 const ComingSoon = () => {
+  const {
+    config: { theme }
+  } = useAppContext();
   return (
-    <Row className="align-items-censter content-min-h">
-      <Col xs={12} lg={6}>
-        <div style={{ height: '40vh' }}>
-          <Lottie
-            animationData={comingSoonLight}
-            loop={true}
-            style={{ width: '100%', height: '100%' }}
-          />
-        </div>
-      </Col>
-      <Col xs={12} lg={6}>
-        <div className="d-flex justify-content-end">
-          <div style={{ maxWidth: 618 }}>
-            <div className="position-relative mb-lg-15">
-              <h1
-                className="display-1 fw-black text-info-200"
-                style={{ transform: 'rotate(1deg)' }}
-              >
-                Coming Soon!!!
-              </h1>
-              <h1
-                className="display-1 fw-black text-primary position-absolute top-0 start-0"
-                style={{ transform: 'rotate(-3deg)' }}
-              >
-                Coming Soon!!!
-              </h1>
-            </div>
-            <h2 className="text-gray-800 mb-3">This page is under development.</h2>
-            <p className="mb-6">
-              This page is under development. We're making it a top priority to get all the pages
-              from the newest HTML version of Phoenix over to the React JS version. Let's go!
-            </p>
-            <div className="d-flex flex-wrap justify-content-center gap-4">
-              <Button style={{ minWidth: 200 }}>
-                <FontAwesomeIcon icon="angle-left" className="me-2" />
-                Getting Started
-              </Button>
-              <Button variant="outline-primary" style={{ minWidth: 200 }}>
-                Preview HTML Demo
-                <FontAwesomeIcon icon="arrow-up-right-from-square" className="ms-2 fs-10" />
-              </Button>
-            </div>
-          </div>
-        </div>
-      </Col>
-      {/* <Col> */}
-    </Row>
+    <div className="d-flex justify-content-center">
+      <Row className="flex-center coming-soon">
+        <Col xs={12} xxl={10}>
+          <Row className="align-items-xl-center pb-9 g-2">
+            <Col xs={12} xl={6} className="order-xl-1">
+              <div className="d-flex flex-center">
+                <Lottie
+                  animationData={theme === 'light' ? comingSoonLight : comingSoonDark}
+                  loop={true}
+                  className="w-xl-100 animation"
+                />
+              </div>
+            </Col>
+            <Col xs={12} xl={6}>
+              <div className="d-flex justify-content-center mt-xl-15">
+                <div className="text-container text-center text-xl-start">
+                  <img
+                    src={comingSoonText}
+                    alt=""
+                    className="mb-5 w-100 w-xl-75 w-xxl-100 d-dark-none"
+                    style={{ maxWidth: 408 }}
+                  />
+                  <img
+                    src={comingSoonTextDark}
+                    alt=""
+                    className="mb-5 w-100 w-xl-75 w-xxl-100 d-light-none"
+                    style={{ maxWidth: 408 }}
+                  />
+
+                  <h2 className="text-gray-800 fs-xl-6 mb-3">This page is under development.</h2>
+                  <p className="mb-6">
+                    This page is under development. We're making it a top priority to get all the
+                    pages from the newest HTML version of Phoenix over to the React JS version.
+                    Let's go!
+                  </p>
+                  <Link
+                    className="btn btn-primary"
+                    to="documentation/getting-started"
+                    style={{ minWidth: 200 }}
+                  >
+                    Getting Started
+                  </Link>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
