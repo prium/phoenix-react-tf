21c133a0c506c68e7571764609008fefafe6a561
Mizan-Rifat <mizanrifat001@gmail.com>
2023-05-23 17:04:11 +0600

    wip

--- src/pages/dashboard/ProjectManagement.tsx
+++ src/pages/dashboard/ProjectManagement.tsx
@@ -1,11 +1,8 @@
+import DatePicker from 'components/base/DatePicker';
 import React, { useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 
 const ProjectManagement = () => {
-  const [startDate, setStartDate] = useState<Date | null>(new Date());
-
   return (
     <Row className="gy-3 justify-content-between">
       <Col md={9} xs="auto">
@@ -13,7 +10,7 @@ const ProjectManagement = () => {
         <h5 className="text-700 fw-semi-bold">Here’s what’s going on at your business right now</h5>
       </Col>
       <Col md={3} xs="auto">
-        <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+        <DatePicker />
       </Col>
     </Row>
   );
