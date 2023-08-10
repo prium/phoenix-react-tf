635ed087488fb4932890f15357f26df6f397de26
Mizan-Rifat <mizanrifat001@gmail.com>
2023-08-10 15:55:30 +0600

    showcase done

--- src/components/navbars/ShowcaseNavbar.tsx
+++ src/components/navbars/ShowcaseNavbar.tsx
@@ -1,11 +1,17 @@
 import Button from 'components/base/Button';
 import Logo from 'components/common/Logo';
+import useNavbarBgChangeOnScroll from 'hooks/useNavbarBgChangeOnScroll';
+import { useRef } from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const ShowcaseNavbar = () => {
+  const navbarRef = useRef(null);
+
+  useNavbarBgChangeOnScroll(navbarRef);
+
   return (
-    <Navbar expand="lg" sticky="top" className="py-3">
+    <Navbar ref={navbarRef} expand="lg" sticky="top" className="py-3">
       <div className="container-small px-0 px-sm-3">
         <Navbar.Brand as={Link} to="/">
           <Logo />
