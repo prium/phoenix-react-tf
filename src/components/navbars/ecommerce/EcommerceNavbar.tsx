a8b3d34ab73b8c118e7c052780de575633fa4dcb
Mizan-Rifat <mizanrifat001@gmail.com>
2023-08-24 00:02:08 +0600

    wip

--- src/components/navbars/ecommerce/EcommerceNavbar.tsx
+++ src/components/navbars/ecommerce/EcommerceNavbar.tsx
@@ -109,95 +109,93 @@ const EcommerceNavbar = () => {
     setDropdownItems(items);
   }, [navItems]);
   return (
-    <Navbar className="ecommerce-navbar bg-white justify-content-between py-0">
-      <div className="container-small">
-        <div
-          className="d-flex flex-between-center flex-nowrap w-100"
-          ref={containerRef}
-        >
-          <Dropdown ref={otherElsRef}>
-            <Dropdown.Toggle
-              variant=""
-              className="text-900 ps-0 pe-5 text-nowrap dropdown-toggle dropdown-caret-none"
-            >
-              <FontAwesomeIcon icon="bars" className="me-2" />
-              Category
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu className="border py-0 category-dropdown-menu ms-2">
-              <Card className="border-0" style={{ maxHeight: 657 }}>
-                <Card.Body className="p-6 pb-3">
-                  <Row className="gx-7 gy-5 mb-5">
-                    {categories.map(category => (
-                      <Col xs={12} sm={6} md={4} key={category.title}>
-                        <div className="d-flex align-items-center mb-3">
-                          <FeatherIcon
-                            icon={category.icon}
-                            className="text-primary me-2"
-                            style={{ strokeWidth: 3 }}
-                            size={16}
-                          />
-                          <h6 className="text-1000 mb-0 text-nowrap">
-                            {category.title}
-                          </h6>
-                        </div>
-                        <div className="ms-n2">
-                          {category.sections.map(section => (
-                            <Link
-                              key={section.label}
-                              to={section.url}
-                              className="text-black d-block mb-1 text-decoration-none hover-bg-100 px-2 py-1 rounded-2"
-                            >
-                              {section.label}
-                            </Link>
-                          ))}
-                        </div>
-                      </Col>
-                    ))}
-                  </Row>
-                  <div className="text-center border-top pt-3">
-                    <Link className="fw-bold" to="#!">
-                      See all Categories
-                      <FontAwesomeIcon icon="angle-right" className="ms-1" />
-                    </Link>
-                  </div>
-                </Card.Body>
-              </Card>
-            </Dropdown.Menu>
-          </Dropdown>
-          <Nav
-            as="ul"
-            className="justify-content-end align-items-center gap-5"
-            ref={navbarRef}
+    <Navbar className="ecommerce-navbar bg-white justify-content-between p-0">
+      <div
+        className="container-small d-flex flex-between-center flex-nowrap w-100"
+        ref={containerRef}
+      >
+        <Dropdown ref={otherElsRef}>
+          <Dropdown.Toggle
+            variant=""
+            className="text-900 ps-0 pe-5 text-nowrap dropdown-toggle dropdown-caret-none"
           >
-            {navItems.map(item => (
-              <Nav.Item className="gap-3" key={item.id}>
-                <Nav.Link key={item.id} as={Link} to={item.url}>
-                  {item.label}
-                </Nav.Link>
-              </Nav.Item>
-            ))}
-            {dropdownItems.length > 0 && (
-              <Dropdown align="end" as={NavItem}>
-                <Dropdown.Toggle
-                  variant=""
-                  className="fw-bold nav-link dropdown-caret-none"
-                >
-                  More
-                  <FontAwesomeIcon icon="angle-down" className="ms-2" />
-                </Dropdown.Toggle>
+            <FontAwesomeIcon icon="bars" className="me-2" />
+            Category
+          </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                  {dropdownItems.map(item => (
-                    <Dropdown.Item key={item.id} as={Link} to={item.url}>
-                      {item.label}
-                    </Dropdown.Item>
+          <Dropdown.Menu className="border py-0 category-dropdown-menu ms-2">
+            <Card className="border-0" style={{ maxHeight: 657 }}>
+              <Card.Body className="p-6 pb-3">
+                <Row className="gx-7 gy-5 mb-5">
+                  {categories.map(category => (
+                    <Col xs={12} sm={6} md={4} key={category.title}>
+                      <div className="d-flex align-items-center mb-3">
+                        <FeatherIcon
+                          icon={category.icon}
+                          className="text-primary me-2"
+                          style={{ strokeWidth: 3 }}
+                          size={16}
+                        />
+                        <h6 className="text-1000 mb-0 text-nowrap">
+                          {category.title}
+                        </h6>
+                      </div>
+                      <div className="ms-n2">
+                        {category.sections.map(section => (
+                          <Link
+                            key={section.label}
+                            to={section.url}
+                            className="text-black d-block mb-1 text-decoration-none hover-bg-100 px-2 py-1 rounded-2"
+                          >
+                            {section.label}
+                          </Link>
+                        ))}
+                      </div>
+                    </Col>
                   ))}
-                </Dropdown.Menu>
-              </Dropdown>
-            )}
-          </Nav>
-        </div>
+                </Row>
+                <div className="text-center border-top pt-3">
+                  <Link className="fw-bold" to="#!">
+                    See all Categories
+                    <FontAwesomeIcon icon="angle-right" className="ms-1" />
+                  </Link>
+                </div>
+              </Card.Body>
+            </Card>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Nav
+          as="ul"
+          className="justify-content-end align-items-center gap-5"
+          ref={navbarRef}
+        >
+          {navItems.map(item => (
+            <Nav.Item className="gap-3" key={item.id}>
+              <Nav.Link key={item.id} as={Link} to={item.url}>
+                {item.label}
+              </Nav.Link>
+            </Nav.Item>
+          ))}
+          {dropdownItems.length > 0 && (
+            <Dropdown align="end" as={NavItem}>
+              <Dropdown.Toggle
+                variant=""
+                className="fw-bold nav-link dropdown-caret-none"
+              >
+                More
+                <FontAwesomeIcon icon="angle-down" className="ms-2" />
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                {dropdownItems.map(item => (
+                  <Dropdown.Item key={item.id} as={Link} to={item.url}>
+                    {item.label}
+                  </Dropdown.Item>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          )}
+        </Nav>
       </div>
     </Navbar>
   );
--- src/pages/HomePage.tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const HomePage = () => {
-  return <Navigate to="/pages/starter" replace={true} />;
-};
-
-export default HomePage;
