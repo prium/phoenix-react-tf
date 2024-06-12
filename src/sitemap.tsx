84b81987d767fe8ee9c97e6856e28355880f0f0b
syum-akhlaque <arssyum@gmail.com>
2024-06-12 14:05:24 +0600

    wip

--- src/sitemap.tsx
+++ src/sitemap.tsx
@@ -71,6 +71,7 @@ export const routes: RouteItems[] = [
           },
           {
             name: 'Travel Agency',
+            new: true,
             path: '/dashboard/travel-agency',
             pathName: 'travel-agency',
             topNavIcon: 'phone',
@@ -320,6 +321,7 @@ export const routes: RouteItems[] = [
       },
       {
         name: 'travel-agency',
+        new: true,
         icon: 'clipboard',
         active: true,
         pages: [
@@ -330,10 +332,64 @@ export const routes: RouteItems[] = [
             active: true
           },
           {
-            name: 'project-list-view',
-            path: '/apps/project-management/project-list-view',
-            pathName: 'project-management-project-list-view',
-            active: true
+            name: 'hotel',
+            pages: [
+              {
+                name: 'admin',
+                pages: [
+                  {
+                    name: 'add-property',
+                    path: 'apps/travel-agency/hotel/admin/add-property',
+                    pathName: 'travel-agency-add-property'
+                  },
+                  {
+                    name: 'add-room',
+                    path: 'apps/travel-agency/hotel/admin/add-room',
+                    pathName: 'travel-agency-add-room'
+                  },
+                  {
+                    name: 'room-listing',
+                    path: 'apps/travel-agency/hotel/admin/room-listing',
+                    pathName: 'travel-agency-room-listing'
+                  },
+                  {
+                    name: 'search-room',
+                    path: 'apps/travel-agency/hotel/admin/room-search',
+                    pathName: 'travel-agency-room-search'
+                  }
+                ]
+              },
+              {
+                name: 'customer',
+                pages: [
+                  {
+                    name: 'homepage',
+                    path: 'apps/travel-agency/hotel/customer/homepage',
+                    pathName: 'hotel-homepage'
+                  },
+                  {
+                    name: 'hotel-details',
+                    path: 'apps/travel-agency/hotel/customer/hotel-details',
+                    pathName: 'hotel-hotel-details'
+                  },
+                  {
+                    name: 'hotel-compare',
+                    path: 'apps/travel-agency/hotel/customer/hotel-compare',
+                    pathName: 'hotel-hotel-compare'
+                  },
+                  {
+                    name: 'checkout',
+                    path: 'apps/travel-agency/hotel/customer/checkout',
+                    pathName: 'hotel-checkout'
+                  },
+                  {
+                    name: 'payment',
+                    path: 'apps/travel-agency/hotel/customer/payment',
+                    pathName: 'hotel-payment'
+                  }
+                ]
+              }
+            ]
           }
         ]
       },
