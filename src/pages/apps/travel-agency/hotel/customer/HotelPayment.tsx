c04919043d45a9507ac9463ca51256371a31dc5e
Sajal13 <sajaldas138900@gmail.com>
2024-09-12 16:28:04 +0600

    Payment form added

--- src/pages/apps/travel-agency/hotel/customer/HotelPayment.tsx
+++ src/pages/apps/travel-agency/hotel/customer/HotelPayment.tsx
@@ -9,12 +9,10 @@ import PageBreadcrumb from 'components/common/PageBreadcrumb';
 import { defaultBreadcrumbItems } from 'data/commonData';
 import { Container, Row, Col, Form } from 'react-bootstrap';
 import HotelDetailsSummaryCard from 'components/cards/HotelDetailsSummaryCard';
+import PaymentMethodForm from 'components/forms/PaymentMethodForm';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
-import visaCardImage from 'assets/img/logos/visa.png';
-import discoverImage from 'assets/img/logos/discover.png';
-import masterCardImage from 'assets/img/logos/mastercard.png';
-import americanExpressImage from 'assets/img/logos/american_express.png';
+
 import Button from 'components/base/Button';
 import { Link } from 'react-router-dom';
 
@@ -25,9 +23,6 @@ const HotelPayment = () => {
     disableVerticalNavbarAppearance: true,
     disableHorizontalNavbarShape: true
   });
-
-  const start_year = 1990;
-  const end_year = new Date().getFullYear();
 
   return (
     <>
@@ -98,183 +93,7 @@ const HotelPayment = () => {
                     />
                   </Col>
                 </Row>
-                <h3 className="mt-7 mb-5">Payment Method</h3>
-                <Row className="gx-xxl-6 mb-5">
-                  <Col md="auto">
-                    <div className="d-flex">
-                      <Form.Check>
-                        <Form.Check.Input
-                          type="radio"
-                          name="paymentMethod"
-                          id="creditCard"
-                          defaultChecked
-                        />
-                        <Form.Check.Label
-                          className="fs-8 text-body me-3"
-                          htmlFor="creditCard"
-                        >
-                          Credit card
-                        </Form.Check.Label>
-                      </Form.Check>
-                      <img
-                        src={visaCardImage}
-                        alt=""
-                        className="h-100 me-2 ms-4 ms-md-0"
-                      />
-                      <img src={discoverImage} alt="" className="h-100 me-2" />
-                      <img
-                        src={masterCardImage}
-                        alt=""
-                        className="h-100 me-2"
-                      />
-                      <img
-                        src={americanExpressImage}
-                        alt=""
-                        className="h-100"
-                      />
-                    </div>
-                  </Col>
-                  <Col md="auto">
-                    <Form.Check>
-                      <Form.Check.Input
-                        type="radio"
-                        name="paymentMethod"
-                        id="paypal"
-                      />
-                      <Form.Check.Label
-                        className="fs-8 text-body"
-                        htmlFor="paypal"
-                      >
-                        Paypal
-                      </Form.Check.Label>
-                    </Form.Check>
-                  </Col>
-                  <Col md="auto">
-                    <Form.Check>
-                      <Form.Check.Input
-                        type="radio"
-                        name="paymentMethod"
-                        id="coupon"
-                      />
-                      <Form.Check.Label
-                        className="fs-8 text-body"
-                        htmlFor="coupon"
-                      >
-                        Coupon
-                      </Form.Check.Label>
-                    </Form.Check>
-                  </Col>
-                </Row>
-                <Row className="gx-3 gy-4">
-                  <Col md={6}>
-                    <label
-                      htmlFor="selectCard"
-                      className="fw-bold text-body-highlight mb-1"
-                    >
-                      Select card
-                    </label>
-                    <Form.Select
-                      className="text-body-emphasis"
-                      id="selectCard"
-                      defaultValue=""
-                    >
-                      <option value="">Select a card</option>
-                      <option value="visa">Visa</option>
-                      <option value="discover">Discover</option>
-                      <option value="mastercard">Mastercard</option>
-                      <option value="american-express">American Express</option>
-                    </Form.Select>
-                  </Col>
-                  <Col md={6}>
-                    <label
-                      htmlFor="inputCardNumber"
-                      className="fw-bold text-body-highlight mb-1"
-                    >
-                      Card number
-                    </label>
-                    <Form.Control
-                      type="number"
-                      placeholder="Enter card number"
-                      aria-label="Card number"
-                      id="inputCardNumber"
-                      className="input-spin-none"
-                    />
-                  </Col>
-                  <Col xs={12}>
-                    <label
-                      htmlFor="inputName"
-                      className="fw-bold text-body-highlight mb-1"
-                    >
-                      Full name
-                    </label>
-                    <Form.Control
-                      type="text"
-                      name="inputName"
-                      placeholder="Ansolo Lazinatov"
-                      id="inputName"
-                      aria-label="Full name"
-                    />
-                  </Col>
-                  <Col md={6}>
-                    <label className="fw-bold text-body-highlight mb-1">
-                      Expires on
-                    </label>
-                    <div className="d-flex">
-                      <Form.Select
-                        className="text-body-emphasis me-3"
-                        defaultValue=""
-                      >
-                        <option value="">Month</option>
-                        <option value="january">January</option>
-                        <option value="february">February</option>
-                        <option value="march">March</option>
-                      </Form.Select>
-                      <Form.Select
-                        className="text-body-emphasis"
-                        defaultValue=""
-                      >
-                        <option value="">Year</option>
-                        {Array.from({ length: end_year - start_year + 1 }).map(
-                          (_, index) => {
-                            const year = start_year + index;
-                            return (
-                              <option key={year} value={year}>
-                                {year}
-                              </option>
-                            );
-                          }
-                        )}
-                      </Form.Select>
-                    </div>
-                  </Col>
-                  <Col md={6}>
-                    <label
-                      htmlFor="inputCardCVC"
-                      className="fw-bold text-body-highlight mb-1"
-                    >
-                      CVC
-                    </label>
-                    <Form.Control
-                      type="number"
-                      name="CVC"
-                      id="inputCardCVC"
-                      placeholder="Enter a valid CVC"
-                      aria-label="CVC"
-                      className="input-spin-none"
-                    />
-                  </Col>
-                  <Col xs={12}>
-                    <Form.Check>
-                      <Form.Check.Input type="checkbox" id="gridCheck" />
-                      <Form.Check.Label
-                        className="text-body-emphasis fs-8"
-                        htmlFor="gridCheck"
-                      >
-                        Save Card Details
-                      </Form.Check.Label>
-                    </Form.Check>
-                  </Col>
-                </Row>
+                <PaymentMethodForm />
                 <hr className="mt-6 mb-5" />
                 <Link
                   to="/apps/travel-agency/hotel/customer/checkout"
