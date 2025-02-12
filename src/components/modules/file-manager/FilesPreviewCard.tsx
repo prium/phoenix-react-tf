a7c1c95969bf719da9562f3d07288a0b7b2c6c7f
syum-akhlaque <arssyum@gmail.com>
2025-02-12 13:28:08 +0600

    lightbox hook updated, filemanager react cards

--- src/components/modules/file-manager/FilesPreviewCard.tsx
+++ src/components/modules/file-manager/FilesPreviewCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const FilesPreviewCard = () => {
-  return <div>FilesPreviewCard</div>;
+  return <div className="bg-white mt-5 p-5">FilesPreviewCard</div>;
 };
 
 export default FilesPreviewCard;
--- src/components/modules/file-manager/RecentFilesCard.tsx
+++ src/components/modules/file-manager/RecentFilesCard.tsx
@@ -1,7 +1,343 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Card, Col, Collapse, Dropdown, Row } from 'react-bootstrap';
+import fileManager1 from 'assets/img/file-manager/1.png';
+import fileManager2 from 'assets/img/file-manager/2.png';
+import fileManager6 from 'assets/img/file-manager/6.png';
+import fileManager7 from 'assets/img/file-manager/7.png';
+import generic67Pdf from 'assets/img/generic/67.pdf';
+import video1 from 'assets/video/1.mp4';
+import video2 from 'assets/video/2.mp4';
+import video3 from 'assets/video/3.mp4';
+import video4 from 'assets/video/4.mp4';
+import Badge from 'components/base/Badge';
+import Lightbox from 'components/base/LightBox';
+import useLightbox from 'hooks/useLightbox';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faChevronDown,
+  faChevronUp,
+  faEllipsisVertical,
+  faPause,
+  faPlay
+} from '@fortawesome/free-solid-svg-icons';
+import Button from 'components/base/Button';
+
+interface RecentFiles {
+  name: string;
+  type: 'image' | 'pdf' | 'video';
+  size: string;
+  createdAt: string;
+  actionType: string;
+  img?: string;
+  pdf?: string;
+  video?: string;
+}
+
+const recentFiles: RecentFiles[] = [
+  {
+    name: 'Verification Letter.pdf',
+    type: 'pdf',
+    size: '8.8 mb',
+    createdAt: '5 min ago',
+    actionType: 'Edited',
+    img: fileManager1,
+    pdf: generic67Pdf
+  },
+  {
+    name: 'Travel Tales.mp4',
+    type: 'video',
+    size: '65 mb',
+    createdAt: '6 min ago',
+    actionType: 'Upload',
+    video: video2
+  },
+  {
+    name: 'Mountain During.jpg',
+    type: 'image',
+    size: '5.8 mb',
+    createdAt: '6 min ago',
+    actionType: 'Upload',
+    img: fileManager2
+  },
+  {
+    name: 'Cinematic Adventures.mp4',
+    type: 'video',
+    size: '54 mb',
+    createdAt: '8 min ago',
+    actionType: 'Upload',
+    video: video3
+  },
+  {
+    name: 'Travel Tales.mp4',
+    type: 'video',
+    size: '65 mb',
+    createdAt: '6 min ago',
+    actionType: 'Upload',
+    video: video1
+  },
+  {
+    name: 'Hot air balloons.png',
+    type: 'image',
+    size: '5.8 mb',
+    createdAt: '6 min ago',
+    actionType: 'Upload',
+    img: fileManager6
+  },
+  {
+    name: 'Aurora Borealis.jpg',
+    type: 'image',
+    size: '10 mb',
+    createdAt: '5 min ago',
+    actionType: 'Edited',
+    img: fileManager7
+  },
+  {
+    name: 'Cinematic Adventures.mp4',
+    type: 'video',
+    size: '54 mb',
+    createdAt: '8 min ago',
+    actionType: 'Upload',
+    video: video4
+  }
+];
+
+const dropdownItems: string[] = [
+  'Share',
+  'Download',
+  'Duplicate',
+  'Move',
+  'Rename',
+  'Move to Bin',
+  'Delete'
+];
+
+const RecentFilesCardItem = ({
+  file,
+  index,
+  openLightbox
+}: {
+  file: RecentFiles;
+  index: number;
+  openLightbox: (index: number) => void;
+}) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      isPlaying ? videoRef.current.play() : videoRef.current.pause();
+    }
+  }, [isPlaying]);
+
+  const handlePlayPause = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  return (
+    <Col sm={6} xxl={3} key={index}>
+      <div
+        className="position-relative"
+        {...(file.type === 'video' && {
+          onMouseEnter: () => setIsPlaying(true),
+          onMouseLeave: () => setIsPlaying(false)
+        })}
+      >
+        <div className="img-zoom-hover mask-image-none overflow-hidden border rounded-3">
+          <div className="position-relative">
+            <div className="mask-image-recent-file overflow-hidden">
+              <div className="ratio ratio-16x9">
+                {(file.type === 'image' || file.type === 'pdf') && (
+                  <img
+                    src={file.img}
+                    alt=""
+                    className="w-100 h-100 object-fit-cover"
+                  />
+                )}
+                {file.type === 'video' && (
+                  <>
+                    <video
+                      className="video d-block h-100 w-100 object-fit-cover"
+                      loop
+                      muted
+                      src={file.video}
+                      ref={videoRef}
+                    />
+                    <Button
+                      className="btn p-0 circle-icon-item-md position-absolute top-50 start-50 translate-middle bg-body-emphasis bg-opacity-50 z-5"
+                      onClick={handlePlayPause}
+                    >
+                      <span className="play-icon pointer-events-none">
+                        {!isPlaying ? (
+                          <FontAwesomeIcon
+                            icon={faPlay}
+                            className="text-body-secondary fs-9"
+                            transform="down-1"
+                          />
+                        ) : (
+                          <FontAwesomeIcon
+                            icon={faPause}
+                            className="text-body-secondary fs-9"
+                            transform="down-1"
+                          />
+                        )}
+                      </span>
+                    </Button>
+                  </>
+                )}
+              </div>
+              <Badge
+                bg="warning"
+                variant="phoenix"
+                className="fs-10 position-absolute top-0 start-0 mt-3 ms-3"
+              >
+                {file.actionType}
+              </Badge>
+            </div>
+          </div>
+          <div className="bg-body p-3 pe-2 d-flex justify-content-between align-items-start rounded-bottom-3">
+            <div className="w-75">
+              <Link
+                to="#!"
+                className="stretched-link text-body-highlight fw-bold mb-2 d-block text-truncate"
+                onClick={() => openLightbox(index + 1)}
+              >
+                {file.name}
+              </Link>
+              <h6 className="mb-0 fw-semibold text-body-tertiary">
+                {file.size}
+              </h6>
+            </div>
+            <Dropdown className="position-static">
+              <Dropdown.Toggle
+                variant=""
+                className="btn-square-sm position-relative mt-n1 dropdown-caret-none z-5"
+              >
+                <FontAwesomeIcon icon={faEllipsisVertical} />
+              </Dropdown.Toggle>
+              <Dropdown.Menu className="py-2" style={{ zIndex: 6 }}>
+                {dropdownItems.map((item, idx) => (
+                  <React.Fragment key={idx}>
+                    {idx < dropdownItems.length - 1 ? (
+                      <Dropdown.Item
+                        href="#!"
+                        className="fw-semibold text-decoration-none"
+                      >
+                        {item}
+                      </Dropdown.Item>
+                    ) : (
+                      <>
+                        <hr className="dropdown-divider" />
+                        <Dropdown.Item
+                          href="#!"
+                          className="fw-semibold text-decoration-none text-danger"
+                        >
+                          Delete
+                        </Dropdown.Item>
+                      </>
+                    )}
+                  </React.Fragment>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        </div>
+      </div>
+    </Col>
+  );
+};
 
 const RecentFilesCard = () => {
-  return <div className="mt-10">RecentFiles</div>;
+  const [attachments] = useState([
+    <iframe
+      key="pdf"
+      src={generic67Pdf}
+      title="PDF Viewer"
+      width="1900px"
+      height="1920px"
+    />,
+    video2,
+    fileManager2,
+    video3
+  ]);
+  // const attachments = recentFiles
+  //   .map(file => {
+  //     if (file.type === 'pdf' && file.pdf) {
+  //       return (
+  //         <iframe
+  //           key={file.name}
+  //           src={file.pdf}
+  //           title="PDF Viewer"
+  //           width="1900px"
+  //           height="1920px"
+  //         />
+  //       );
+  //     }
+  //     if (file.type === 'video' && file.video) {
+  //       return file.video;
+  //     }
+  //     if (file.type === 'image' && file.img) {
+  //       return file.img;
+  //     }
+  //     return '';
+  //   })
+  //   .filter(attachment => attachment === ''); // Remove empty values
+
+  attachments.length && console.log(attachments);
+  const { lightboxProps, openLightbox } = useLightbox([...attachments]);
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  return (
+    <Card className="mt-4">
+      <Lightbox {...lightboxProps} />
+      <Card.Body className="pb-5">
+        <h4 className="mb-3">Recent Files</h4>
+        <Row className="g-3">
+          {recentFiles.slice(0, 4).map((file, index) => (
+            <RecentFilesCardItem
+              key={index}
+              file={file}
+              index={index}
+              openLightbox={openLightbox}
+            />
+          ))}
+        </Row>
+        <Collapse in={!isCollapsed}>
+          <div className="mt-3">
+            <Row className="g-3">
+              {recentFiles.slice(4, 8).map((file, index) => (
+                <RecentFilesCardItem
+                  key={index}
+                  file={file}
+                  index={index}
+                  openLightbox={openLightbox}
+                />
+              ))}
+            </Row>
+          </div>
+        </Collapse>
+        <Button
+          variant="phoenix-secondary"
+          className="btn collapse-indicator bg-body-emphasis fs-10 py-1 border rounded-1 px-3 position-absolute start-50 translate-middle-x"
+          style={{ bottom: '-11px' }}
+          role="button"
+          onClick={toggleCollapse}
+        >
+          <span className={isCollapsed ? 'collapse-show' : 'collapse-hide'}>
+            {isCollapsed ? 'VIEW MORE' : 'VIEW LESS'}
+          </span>
+          <FontAwesomeIcon
+            icon={isCollapsed ? faChevronDown : faChevronUp}
+            className="toggle-icon fs-10 ms-2"
+          />
+        </Button>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default RecentFilesCard;
